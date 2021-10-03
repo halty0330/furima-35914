f0f6aa14d7b10eb243c3969f412c406907e0529e
halty0330 <hal0330t0321@icloud.com>
2021-10-04 06:07:13 +0900

    テストコードの追加（コードレビュー指摘箇所、CSS配置）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_furigana, presence: true
-  validates :first_name_furigana, presence: true
-  validates :birthday, presence: true
+
+  with_options presence: true do
+   validates :nickname
+   validates :last_name,format: {with: /\A[ぁ-んァ-ン一-龥]/}
+   validates :first_name,format: {with: /\A[ぁ-んァ-ン一-龥]/}
+   validates :last_name_furigana,format: {with: /\A[ァ-ヶー－]+\z/ }
+   validates :first_name_furigana,format: {with: /\A[ァ-ヶー－]+\z/ }
+   validates :password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+   validates :birthday
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-     it'nickname、last_name、first_name、last_name_furigana、first_name_furigana、birthday、email、passwordが存在すれば登録できる' do
+     it'nickname、last_name、first_name、last_name_furigana、first_name_furigana、birthday、email、password、password_confirmationが存在すれば登録できる' do
       expect(@user).to be_valid
      end
      it 'passwordが6文字以上であれば登録できる' do
-      @user.password = '123456'
-      @user.password_confirmation = '123456'
+      @user.password = 'ty630321'
+      @user.password_confirmation = 'ty630321'
       expect(@user).to be_valid
      end
     end
@@ -42,6 +42,26 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("First name furigana can't be blank")
     end
+    it 'last_nameに半角文字が含まれていると登録できない' do
+      @user.last_name = 'ｱｲｳｴｵ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name is invalid")
+    end
+    it 'first_nameに半角文字が含まれていると登録できない' do
+      @user.first_name = 'ｱｲｳｴｵ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name is invalid")
+    end
+    it 'last_name_furiganaにカタカナ以外の文字が含まれていると登録できない' do
+      @user.last_name_furigana = 'あいうえお'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name furigana is invalid")
+    end
+    it 'first_name_furiganaにカタカナ以外の文字が含まれていると登録できない' do
+      @user.first_name_furigana = 'あいうえお'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name furigana is invalid")
+    end
     it 'birthdayが空だと登録できない' do
       @user.birthday = ''
       @user.valid?
@@ -51,6 +71,11 @@ RSpec.describe User, type: :model do
       @user.email = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'emailに@が含まれていない場合登録できない' do
+      @user.email = 'hal0330.com'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
     end
     it 'passwordが空だと登録できない' do 
       @user.password = ''
@@ -68,6 +93,21 @@ RSpec.describe User, type: :model do
       @user.password_confirmation = '1234567'
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+    it '英字のみのpasswordであれば登録できない' do 
+      @user.password = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid")
+    end
+    it '数字のみのpasswordであれば登録できない' do 
+      @user.password = '111111'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid") 
+    end
+    it '全角文字のみのpasswordであれば登録できない' do 
+      @user.password = 'ＡＡＡＡＡＡ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid") 
     end
     it '重複したemailが存在する場合登録できない' do
       @user.save
