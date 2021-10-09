e9af638b820412593d602e1dbc9dedf249859b8a
halty0330 <hal0330t0321@icloud.com>
2021-10-09 17:38:31 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,27 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :scheduled_day
 
-
   has_one_attached :image
+
+
+  validates :item_name, :string, presence: true
+  validates :explanation, :text, presence: true
+  validates :category_id, :integer, presence: true
+  validates :condition_id, :integer, presence: true
+  validates :fee_id, :integer, presence: true
+  validates :condition_id, :integer, presence: true
+  validates :area_id, :integer, presence: true
+  validates :scheduled_day_id, :integer, presence: true
+  validates :price, :integer, presence: true
+  validates :image, :string, presence: true
+
+
   
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
