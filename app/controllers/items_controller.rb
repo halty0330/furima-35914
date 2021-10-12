fbf769205c24ed32a268436be755ac3c874def97
halty0330 <hal0330t0321@icloud.com>
2021-10-13 07:05:02 +0900

    テストコードの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,28 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except:[:new]
+
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(item_id: current_user.id)
+    params.require(:item).permit(:item_name, :price, :category_id, :explanation, :condition_id, :fee_id, :area_id, :scheduled_day_id, :image).merge(user_id: current_user.id)
   end
 end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,16 +9,16 @@ class Item < ApplicationRecord
   has_one_attached :image
 
 
-  validates :item_name, :string, presence: true
-  validates :explanation, :text, presence: true
-  validates :category_id, :integer, presence: true
-  validates :condition_id, :integer, presence: true
-  validates :fee_id, :integer, presence: true
-  validates :condition_id, :integer, presence: true
-  validates :area_id, :integer, presence: true
-  validates :scheduled_day_id, :integer, presence: true
-  validates :price, :integer, presence: true
-  validates :image, :string, presence: true
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :fee_id, presence: true
+  validates :condition_id, presence: true
+  validates :area_id, presence: true
+  validates :scheduled_day_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 
 
   
@@ -28,5 +28,7 @@ class Item < ApplicationRecord
   validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :scheduled_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999  , message: "is invalid"}
+
 
 end
