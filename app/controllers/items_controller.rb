87a2a2404c95666d7142e210efaee00042fa3193
halty0330 <hal0330t0321@icloud.com>
2021-10-24 16:18:31 +0900

    ログイン、出品状態の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except:[:index]
+  before_action :authenticate_user!, except:[:index, :show]
 
 
   def index
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @items = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   private
