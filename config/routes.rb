8390280022bc29d7439b7a77300e3f2a9f516d98
halty0330 <hal0330t0321@icloud.com>
2021-10-27 15:47:25 +0900

    削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index"
 
-  resources :items, only: [:new, :create, :index, :show, :edit, :update] do
+  resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
   end
 
 end
