f10401476214f9a0171743b61095979bfec053f1
halty0330 <hal0330t0321@icloud.com>
2021-10-27 12:28:43 +0900

    before_actionの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show ]
+  before_action :set_item, except: [:show, :edit, :update ]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -19,17 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
-
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -43,4 +40,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :price, :category_id, :explanation, :condition_id, :fee_id, :area_id,
                                  :scheduled_day_id, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
